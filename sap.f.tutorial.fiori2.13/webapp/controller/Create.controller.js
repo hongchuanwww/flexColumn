b7c954824c16b10ca1349ddc2204093120ac3dcf
hongchuanwww <49533277+hongchuanwww@users.noreply.github.com>
2022-01-06 00:22:24 -0800

    odata post and paste

--- sap.f.tutorial.fiori2.13/webapp/controller/Create.controller.js
+++ sap.f.tutorial.fiori2.13/webapp/controller/Create.controller.js
@@ -16,11 +16,39 @@ sap.ui.define([
 			this.oRouter.getRoute("create").attachPatternMatched(this._onProductMatched, this);
 			var oNewModel = new JSONModel([]);
 			this.getView().setModel(oNewModel, "new");
-			var pasteButton = this.getView().byId('editButton');
-			var oTable = this.getView().byId('table');
-			pasteButton.addDependent(new PasteProvider({
-				pasteFor: oTable.getId() // Reference to the control the paste is associated with, e.g. a sap.m.Table
-			}));
+			// var pasteButton = this.getView().byId('editButton');
+			// var oTable = this.getView().byId('table');
+			// pasteButton.addDependent(new PasteProvider({
+			// 	pasteFor: oTable.getId() // Reference to the control the paste is associated with, e.g. a sap.m.Table
+			// }));
+		},
+
+		// onPaste: function(oEvent) {
+		// 	if (!oEvent.getSource().getBindingContext().getProperty("READONLY")) {
+		// 		var oItems = oEvent.getParameter("data");
+		// 		oItems.forEach(function(item) {
+		// 			var _oData = {};
+		// 			_oData.PRODUCT_ID = item[0];
+		// 			_oData.QUANTITY = item[1];
+		// 			this.addItemCust(_oData);
+		// 		}.bind(this));
+		// 	}
+		// },
+		firePaste: function(oEvent) {
+			// var oTable1 = this.getView().byId("__table1");
+			navigator.clipboard.readText();
+			// navigator.clipboard.readText().then(
+			// 	function(text) {
+			// 		var _arr = text.split('\r\n');
+			// 		_arr.pop();
+			// 		for (var i in _arr) {
+			// 			_arr[i] = _arr[i].split('\t');
+			// 		}
+			// 		oTable1.firePaste({
+			// 			"data": _arr
+			// 		});
+			// 	}.bind(this)
+			// );
 		},
 
 		onSupplierPress: function (oEvent) {
@@ -55,40 +83,67 @@ sap.ui.define([
 			this.getView().getModel('new').setData(data);
 		},
 
-		onPaste: function (e) {
-			var data = e.getParameters().data;
-			this.getView().getModel('new').setData(data.map(row => {
-				var obj = {};
-				for(var i = 0; i < row.length; i++) {
-					obj['col' + i] = row[i];
-				}
-				return obj;
-			}));
-		}
-		
-		// onSave:  function () {
-		// 	var fnSuccess = function () {
-		// 		this._setBusy(false);
-		// 		MessageToast.show(this._getText("changesSentMessage"));
-		// 		this._setUIChanges(false);
-		// 	}.bind(this);
-
-		// 	var fnError = function (oError) {
-		// 		this._setBusy(false);
-		// 		this._setUIChanges(false);
-		// 		MessageBox.error(oError.message);
-		// 	}.bind(this);
-		// 	// this.getView().getModel().submitBatch("peopleGroup").then(fnSuccess, fnError);
-		// 	var sPath = '/BundleListSet';
-		// 	var mParameters = {
-		// 		context:  {},
-		// 		created: function(a,b,c,d) {
-		// 			console.log(a,b,c,d);
-		// 		},
-		// 		error: fnError,
-		// 		success: fnSuccess
-		// 	};
-		// 	this.getView().getModel('invoice').createEntry(sPath,mParameters);
+		// onPaste: function (e) {
+		// 	var data = e.getParameters().data;
+		// 	this.getView().getModel('new').setData(data.map(row => {
+		// 		var obj = {};
+		// 		for(var i = 0; i < row.length; i++) {
+		// 			obj['col' + i] = row[i];
+		// 		}
+		// 		return obj;
+		// 	}));
 		// },
+		
+		onSave:  function () {
+			var fnSuccess = function () {
+				MessageToast.show('success');
+			}.bind(this);
+
+			var fnError = function (oError) {
+				MessageBox.error(oError.message);
+			}.bind(this);
+			var sPath = 'BundleListSet';
+			var items = [];// intializing an array
+
+			// var aTableData = oTable.getModel().getData();// getting table data
+
+			for (var i = 0; i < 10; i++) {
+
+			items.push( {
+
+				"ItemKey": "ItemKey",
+
+				"ItempartGrp": "ItemKey"
+
+				});
+
+			};
+
+			var oEntry = {
+
+				"ItemKey": "ItemKey",
+
+				"ItempartGrp": "ItemKey",
+			  
+				"Itempart": JSON.parse(JSON.stringify(items)),
+			  
+				//  converting data into json format
+			  
+			  // string type will converted into json object by parsing it.
+			  
+			  };
+
+			  
+
+			var mParameters = {
+				properties:  oEntry,
+
+				error: fnError,
+				success: fnSuccess
+			};
+			
+			var oDataModel = this.getView().getModel('invoice');
+			oDataModel.create(sPath, oEntry, mParameters);
+		},
 	});
 });
