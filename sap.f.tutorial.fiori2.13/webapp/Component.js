880173f8acc0303fddddb7d77d9b21f427b0c2c7
fanhao <fanhao@hecom.cn>
2022-02-21 15:15:46 +0800

    Component.js

--- sap.f.tutorial.fiori2.13/webapp/Component.js
+++ sap.f.tutorial.fiori2.13/webapp/Component.js
@@ -44,13 +44,24 @@ sap.ui.define([
 			// var sServiceUrl = "https://dev.abbott-md.cn:4443/sap/opu/odata/SAP/ZYCHCN_API_ORDER_002_SRV/",
 			// 	bJSON = true,
 			// 	sUser = "huangs03",
-			// 	sPwd = "3edc!QAZ";
+			// 	sPwd = "Qwe@0501";
 			// oODataModel = new sap.ui.model.odata.ODataModel(sServiceUrl, bJSON, sUser, sPwd);
 			this.setModel(oODataModel,'invoice');
-
+			['PROM_TYPE','PROD_SCOPE'].forEach(key => this._getOptions(key));
 			oRouter = this.getRouter();
 			oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
 			oRouter.initialize();
+		},
+
+		_getOptions: function(key) {
+			var that = this;
+			this.getModel('invoice').read('/SHConfigSet?$filter=SubName%20eq%20%27'+key+'%27', {
+				success: function (oData) {
+					var oModel = new JSONModel();
+					oModel.setData(oData.results);
+					that.setModel(oModel,key);
+				}
+			});
 		},
 
 		getHelper: function () {
